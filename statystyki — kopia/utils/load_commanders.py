92e7db83d6bc7f42847c9db2d50b9780b1995ea4
Efk94 <143335916+Efk94@users.noreply.github.com>
2024-08-06 14:57:43 +0000

    Delete statystyki — kopia directory

--- statystyki — kopia/utils/load_commanders.py
+++
@@ -1,7 +0,0 @@
-def load_commanders():
-    avatar_folder = os.path.join(os.path.dirname(__file__), 'Avatar')  # Użyj względnej ścieżki
-    commanders = {}
-    for filename in os.listdir(avatar_folder):
-        name, _ = os.path.splitext(filename)
-        commanders[name] = os.path.join(avatar_folder, filename)
-    return commanders
